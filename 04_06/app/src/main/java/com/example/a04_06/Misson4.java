b6f69adec9092bbbe34de7e003cf1f8437539c34
seyeonJeong <wjdtpdus828@naver.com>
2022-04-07 00:25:34 +0900

    5주차 과제

---
+++ 04_06/app/src/main/java/com/example/a04_06/Misson4.java
@@ -0,0 +1,14 @@
+package com.example.a04_06;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Misson4 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_misson4);
+    }
+}
